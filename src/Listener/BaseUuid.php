2aac843478640dcc82ff450964c146a9138f3ad4
Adam Dyson <adamdyson@pm.me>
2025-11-02 18:34:39 +1100

    Added UUID factory to base UUID Listener

--- src/Listener/BaseUuid.php
+++ src/Listener/BaseUuid.php
@@ -6,9 +6,13 @@ namespace Cycle\ORM\Entity\Behavior\Identifier\Listener;
 
 use Cycle\ORM\Entity\Behavior\Attribute\Listen;
 use Cycle\ORM\Entity\Behavior\Event\Mapper\Command\OnCreate;
+use Ramsey\Identifier\Uuid;
+use Ramsey\Identifier\Uuid\UuidFactory;
 
 abstract class BaseUuid
 {
+    protected UuidFactory $factory;
+
     /**
      * @param non-empty-string $field The name of the field to store the UUID
      * @param bool $nullable Indicates whether the UUID can be null
@@ -16,7 +20,9 @@ abstract class BaseUuid
     public function __construct(
         private readonly string $field,
         private readonly bool $nullable = false,
-    ) {}
+    ) {
+        $this->factory = new UuidFactory();
+    }
 
     #[Listen(OnCreate::class)]
     public function __invoke(OnCreate $event): void
@@ -28,5 +34,5 @@ abstract class BaseUuid
         $event->state->register($this->field, $this->createValue());
     }
 
-    abstract protected function createValue(): \Ramsey\Identifier\Uuid;
+    abstract protected function createValue(): Uuid;
 }
