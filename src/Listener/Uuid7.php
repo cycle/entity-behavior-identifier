3c87203dd8ad42e1d03fe463911c745c29ecb7e2
Adam Dyson <adamdyson@pm.me>
2025-11-02 18:38:33 +1100

    Updated UUIDv7 Listener to use factory, changed return type to more specific value

--- src/Listener/Uuid7.php
+++ src/Listener/Uuid7.php
@@ -4,15 +4,13 @@ declare(strict_types=1);
 
 namespace Cycle\ORM\Entity\Behavior\Identifier\Listener;
 
-use Ramsey\Identifier\Uuid\UuidV7Factory;
+use Ramsey\Identifier\Uuid\UuidV7;
 
 /**
  * Generates UUIDv7 (time-ordered with random data) identifiers for entities.
  */
 final class Uuid7 extends BaseUuid
 {
-    private UuidV7Factory $factory;
-
     /**
      * @param non-empty-string $field The name of the field to store the UUID
      * @param bool $nullable Indicates whether the UUID can be null
@@ -21,13 +19,12 @@ final class Uuid7 extends BaseUuid
         string $field,
         bool $nullable = false,
     ) {
-        $this->factory = new UuidV7Factory();
         parent::__construct($field, $nullable);
     }
 
     #[\Override]
-    protected function createValue(): \Ramsey\Identifier\Uuid
+    protected function createValue(): UuidV7
     {
-        return $this->factory->create();
+        return $this->factory->v7();
     }
 }
