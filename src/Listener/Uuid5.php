ef86bddfae216f07b6fe2a2f3bff110325e5ac4a
Adam Dyson <adamdyson@pm.me>
2025-11-02 18:38:16 +1100

    Updated UUIDv5 Listener to use factory, changed return type to more specific value, added support for default values

--- src/Listener/Uuid5.php
+++ src/Listener/Uuid5.php
@@ -7,34 +7,57 @@ namespace Cycle\ORM\Entity\Behavior\Identifier\Listener;
 use Cycle\ORM\Entity\Behavior\Identifier\Listener\BaseUuid as Base;
 use Ramsey\Identifier\Uuid;
 use Ramsey\Identifier\Uuid\NamespaceId;
-use Ramsey\Identifier\Uuid\UuidFactory;
+use Ramsey\Identifier\Uuid\UuidV5;
 
 /**
  * Generates UUIDv5 (name-based with SHA-1 hashing) identifiers for entities.
  */
 final class Uuid5 extends Base
 {
-    private readonly UuidFactory $factory;
+    private static NamespaceId|Uuid|string|null $defaultNamespace = null;
+    private static ?string $defaultName = null;
 
     /**
      * @param non-empty-string $field The name of the field to store the UUID
-     * @param NamespaceId|Uuid|string $namespace The namespace UUID
-     * @param string $name The name to hash
      * @param bool $nullable Indicates whether the UUID can be null
+     * @param NamespaceId|Uuid|string|null $namespace The namespace UUID
+     * @param string|null $name The name to hash
      */
     public function __construct(
         string $field,
-        private readonly NamespaceId|Uuid|string $namespace,
-        private readonly string $name,
         bool $nullable = false,
+        private readonly NamespaceId|Uuid|string|null $namespace = null,
+        private readonly ?string $name = null,
     ) {
-        $this->factory = new UuidFactory();
         parent::__construct($field, $nullable);
     }
 
+    /**
+     * Set default values for UUIDv3 generation.
+     *
+     */
+    public static function setDefaults(
+        NamespaceId|Uuid|string|null $namespace,
+        ?string $name,
+    ): void {
+        self::$defaultNamespace = $namespace;
+        self::$defaultName = $name;
+    }
+
     #[\Override]
-    protected function createValue(): \Ramsey\Identifier\Uuid
+    protected function createValue(): UuidV5
     {
-        return $this->factory->v5($this->namespace, $this->name);
+        $namespace = $this->namespace ?? self::$defaultNamespace;
+        $name = $this->name ?? self::$defaultName;
+
+        if ($namespace === null) {
+            throw new \InvalidArgumentException('Namespace must be specified.');
+        }
+
+        if ($name === null) {
+            throw new \InvalidArgumentException('Name must be specified.');
+        }
+
+        return $this->factory->v5($namespace, $name);
     }
 }
